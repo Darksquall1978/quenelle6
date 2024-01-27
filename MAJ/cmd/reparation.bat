b062b07c90913161089829b29d30edae647990d1
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2024-01-27 23:59:02 +0100

    Add files via upload

--- MAJ/cmd/reparation.bat
+++ MAJ/cmd/reparation.bat
@@ -18,8 +18,26 @@ RMDIR /s /q FrostyModManager\Mods
 timeout /t 1 /nobreak > nul
 @echo  suppression de KyberBrowser
 RMDIR /s /q KyberBrowser\KyberBrowser.exe
+::timeout /t 1 /nobreak > nul
+::@echo  suppression du dossier patchers
+::RMDIR /s /q bepinex\patchers
+::timeout /t 1 /nobreak > nul
+::@echo  suppression du dossier plugins-off
+::RMDIR /s /q bepinex\plugins-off
+::timeout /t 1 /nobreak > nul
+::@echo  suppression du dossier doorstop_libs
+::RMDIR /s /q doorstop_libs
+::timeout /t 1 /nobreak > nul
 
+::timeout /t 1 /nobreak > nul
+::@echo  suppression du dossier unstripped_corlib
+::RMDIR /s /q unstripped_corlib
+::timeout /t 1 /nobreak > nul
+::@echo  suppression du dossier vplus-data
+::RMDIR /s /q vplus-data
+::timeout /t 1 /nobreak > nul
 
+ 
 @echo  Copie des fichiers source
 
 timeout /t 2 /nobreak > nul
@@ -42,6 +60,58 @@ robocopy maj\cmd\quenelle6\FrostyModManager\Mods FrostyModManager\Mods /mt /z /e
 ::ok
 ::pause
 
+::robocopy maj\cmd\quenelle6\bepinex\patchers BepInEx\patchers /mt /z /e
+::timeout /t 1 /nobreak > nul
+::ok
+::pause
+
+::xcopy maj\cmd\quenelle6\bepinex\plugins BepInEx\plugins\ /y
+::timeout /t 1 /nobreak > nul
+
+::robocopy maj\cmd\quenelle6\bepinex\plugins BepInEx\plugins /mt /z /e
+::timeout /t 1 /nobreak > nul
+::ok
+::pause
+
+::xcopy maj\cmd\quenelle6\bepinex\config BepInEx\config\ /y
+::timeout /t 1 /nobreak > nul
+::ok
+::pause
+
+::xcopy maj\cmd\quenelle6\bepinex\config BepInEx\conf-temp\ /y
+::timeout /t 1 /nobreak > nul
+::ok
+::pause
+
+::robocopy maj\cmd\quenelle6\bepinex\plugins-off BepInEx\plugins-off /mt /z /e
+::timeout /t 1 /nobreak > nul
+::ok
+::pause
+
+::robocopy maj\cmd\quenelle6\doorstop_libs doorstop_libs /mt /z /e
+::timeout /t 1 /nobreak > nul
+::ok
+::pause
+
+::robocopy maj\cmd\quenelle6\launcher launcher /mt /z /e 
+::timeout /t 1 /nobreak > nul
+::ok
+::pause
+
+::robocopy maj\cmd\quenelle6\unstripped_corlib unstripped_corlib /mt /z /e 
+::timeout /t 1 /nobreak > nul
+::ok
+::pause
+
+::robocopy maj\cmd\quenelle6\vplus-data vplus-data /mt /z /e 
+::timeout /t 1 /nobreak > nul
+::ok
+::pause
+
+::xcopy maj\cmd\quenelle6\doorstop_config.ini doorstop_config.ini /y
+::timeout /t 1 /nobreak > nul
+::ok
+::pause
 
 ::robocopy maj\cmd\quenelle6\maj\cmd maj\cmd /mt /z /e 
 ::timeout /t 1 /nobreak > nul
@@ -53,6 +123,10 @@ xcopy maj\cmd\quenelle6\Launcher.apm Launcher.apm /y
 ::ok
 ::pause
 
+::xcopy maj\cmd\quenelle6\winhttp_.dll winhttp_.dll /y
+::timeout /t 1 /nobreak > nul
+::ok
+::pause
 
 ::xcopy maj\cmd\quenelle6\Manager.apm Manager.apm /y
 ::timeout /t 1 /nobreak > nul
@@ -61,6 +135,7 @@ xcopy maj\cmd\quenelle6\Launcher.apm Launcher.apm /y
 @echo off
 IF EXIST maj\cmd\quenelle6 RMDIR /s /q maj\cmd\quenelle6
 
+::start maj\cmd\reparerfin.bat
 
 start launcher.exe
 exit
