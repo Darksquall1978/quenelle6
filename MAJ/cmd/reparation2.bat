e7c1372fcfe4a5b03c234def347501f39b7fa814
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2024-01-28 10:02:21 +0100

    Add files via upload

---
+++ MAJ/cmd/reparation2.bat
@@ -0,0 +1,70 @@
+@echo off
+Color 0A & Mode con cols=120 lines=100
+batbox /c 0X06 /d  
+git clone https://github.com/Darksquall1978/quenelle6.git 
+batbox /c 0X02 /d 
+cd..
+cd..
+@echo off
+
+@echo MAJ du --27/01/2024-- 15H00
+
+timeout /t 2 /nobreak > nul
+@echo  suppression du .git
+RMDIR /s /q .git
+::timeout /t 1 /nobreak > nul
+@echo  suppression du dossier Mods
+RMDIR /s /q FrostyModManager\Mods\starwarsbattlefrontii\IOI - Instant Online Improvements V5.1.fbmod
+timeout /t 1 /nobreak > nul
+@echo  suppression de KyberBrowser
+RMDIR /s /q KyberBrowser\KyberBrowser.exe
+
+ 
+@echo  Copie des fichiers source
+
+timeout /t 2 /nobreak > nul
+
+
+
+@echo off 
+robocopy maj\cmd\quenelle6\.git .git /mt /z /e
+timeout /t 1 /nobreak > nul
+::ok
+::pause
+
+xcopy maj\cmd\quenelle6\KyberBrowser\ KyberBrowser /y
+::timeout /t 1 /nobreak > nul
+::ok
+::pause
+
+robocopy maj\cmd\quenelle6\FrostyModManager\Mods FrostyModManager\Mods /mt /z /e
+::timeout /t 1 /nobreak > nul
+::ok
+::pause
+
+
+::robocopy maj\cmd\quenelle6\maj\cmd maj\cmd /mt /z /e 
+::timeout /t 1 /nobreak > nul
+::ok
+::pause
+
+xcopy maj\cmd\quenelle6\Launcher.apm Launcher.apm /y
+::timeout /t 1 /nobreak > nul
+::ok
+::pause
+
+
+::xcopy maj\cmd\quenelle6\Manager.apm Manager.apm /y
+::timeout /t 1 /nobreak > nul
+
+
+@echo off
+IF EXIST maj\cmd\quenelle6 RMDIR /s /q maj\cmd\quenelle6
+
+::start maj\cmd\reparerfin.bat
+
+start launcher.exe
+exit
+
+
+
